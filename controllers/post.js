00938ebea965a4d7e12c79215afbdd334e33516d
Aman Kumar Tiwari <kumaramantiwari98@gmail.com>
2020-03-19 03:02:31 +0530

    added post routes

--- controllers/post.js
+++ controllers/post.js
@@ -2,8 +2,29 @@ const Post = require('../models/post')
 const formidable = require('formidable')
 const fs = require('fs')
 
+
+
+
+exports.postFindById = (req,res , next , id)=>{
+    Post.findById(id)
+    .populate("postedBy" , "_id name")
+    .exec( (err , post)=>{
+             if(err || !post){
+                 return res.status(400).json({
+                     error:err
+                 })
+             }
+      req.post = post;
+      next();
+    } )
+}
+
+
 exports.getRoutes=(req,res)=>{
-    const post = Post.find().then(posts=>{
+    const post = Post.find()
+    .populate( "postedBy","_id name")
+    .select("_id title body")
+    .then(posts=>{
         return res.status(200).json({
             posts:posts
         });
@@ -43,3 +64,63 @@ exports.createPost=(req,res,next)=>{
 
   } )
 }
+
+exports.postByUser =(req,res)=>{
+
+  Post.find({ postedBy: req.profile._id })
+  .populate("postedBy","_id name")
+  .sort("_created")
+  .exec((err,posts)=>{
+
+      if(err){
+          return res.status(400).json({
+              error:err
+          })
+        }
+        return res.status(200).json(posts);
+  })
+}
+
+exports.isPoster = (req,res)=>{
+
+let isPoster= req.post && req.auth && req.post.postedBy._id === req.auth._id;
+ 
+   if(!isPoster){
+       return res.staus(403).json({
+           msg:'user not authorized',
+       })
+   }
+  next();
+}
+
+
+exports.deletePost = (req,res)=>{
+
+   let post= req.post;
+
+   post.remove((err,post)=>{
+       if(err){
+           return res.status(400).json({
+              error:err
+           })
+       }
+
+       return res.json({
+           msg:"post deleted succesfully"
+       })
+
+   })
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {getRoutes,createPost} = require('../controllers/post');
-const {userFindById} = require('../controllers/user')
-const {requireSignin} = require('../controllers/auth');
-const {createPostValidator} = require('../validators')
+const { getRoutes, createPost, postByUser,postFindById,isPoster,deletePost } = require("../controllers/post");
+const { userFindById } = require("../controllers/user");
+const { requireSignin } = require("../controllers/auth");
+const { createPostValidator } = require("../validators");
+
+router.get("/", getRoutes);
+
+router.post(
+  "/post/new/:userId",
+  requireSignin,
+  createPost,
+  createPostValidator
+);
+
+router.get("/posts/by/:userId", requireSignin , postByUser);
+
+router.delete("post/:postId" ,requireSignin , isPoster , deletePost);
+
+router.param("userId", userFindById);
+router.param("postId", postFindById);
 
 
-router.get('/', getRoutes );
-
-router.post('/post/new/:userId', requireSignin,createPost,createPostValidator);
-
-router.param("userId" , userFindById )
-
-module.exports=router;
+module.exports = router;
