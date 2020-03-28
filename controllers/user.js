85acf782e195035a766a9de64525296574acc233
Aman Kumar Tiwari <kumaramantiwari98@gmail.com>
2020-03-28 23:00:52 +0530

    user methods added

--- controllers/user.js
+++ controllers/user.js
@@ -1,111 +1,175 @@
-const User = require('../models/user');
-const _= require('lodash');
-const formidable = require('formidable')
-const fs = require('fs')
+const User = require("../models/user");
+const _ = require("lodash");
+const formidable = require("formidable");
+const fs = require("fs");
 
-exports.userFindById =(req,res,next,id)=>{
-
-     User.findById(id).exec( (err,user)=>{
-
-          if(err || !user){
-              return res.status(400).json({
-                  "err":"User not found"
-              })
-          }
-
-         req.profile =user;  // add profile information into user object
-         next();
-     } )
-}
-
-exports.hasAutherization = (req,res,next)=>{
-
- const authorized = req.profile && req.auth && req.profile._id===req.auth._id;
-
-   if(!authorized){
-       return res.status(403).json({
-           error:'user not authorized'
-       })
-   }
-}
-
-exports.getAllUsers = (req,res)=>{
-
-    User.find((err,users)=>{
-        if(err){
-            return res.status(400).json({
-                err
-            })
-        }
-        return res.status(200).json({users})
-    }).select("name email created updated")
-}
-
-exports.getUser  = (req,res)=>{
-   // console.log("asd");
-    //console.log(req.profile);
-    return res.status(200).json(req.profile)
-}
-
-exports.updateUser = (req,res)=>{
-   
-  let form = new formidable.IncomingForm();
-  form.keepExtensions=true;
- 
-  form.parse( req , (err,fields,files) =>{
-      if(err){
-          return res.status(400).json({
-              error:"photo caould not be uploaded"
-          })
-      }
-   let user = req.profile
-   user = _.extend(user,fields)
-   user.updated = Date.now();
-
-   if(files.photo){
-       user.photo.data=fs.readFileSync(files.photo.path)
-       user.photo.contentType= files.photo.type 
-   }
-
-    user.save((err,result)=>{
-        if(err){
-            return res.status(400).json({
-                error:err
-            })
-        }
-        user.hashed_password=undefined;
-        user.salt =undefined
-        res.json(user);
-    })
-
-  })
-
-}
-
-exports.userPhoto = (req,res,next)=>{
-
-   if(req.profile.photo.data){
-       res.set(("content-type",req.profile.photo.contentType));
-       return res.send(req.profile.photo.data)
-   }
-   next();
-
-}
-
-
-exports.deleteUser = (req,res)=>{
-    let user = req.profile;
-
-    user.delete((err)=>{
-        if(err){
-            return res.status(401).json({
-                err:'unauthorized'
-            })
-        }
-        return res.status(200).json({
-            msg:'user successfully deleted'
+exports.userFindById = (req, res, next, id) => {
+  User.findById(id)
+    .populate("following", "_id name")
+    .populate("followers", "_id name")
+    .exec((err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          err: "User not found"
         });
-    })
-}
+      }
+
+      req.profile = user; // add profile information into user object
+      next();
+    });
+};
+
+exports.hasAutherization = (req, res, next) => {
+  const authorized =
+    req.profile && req.auth && req.profile._id === req.auth._id;
+
+  if (!authorized) {
+    return res.status(403).json({
+      error: "user not authorized"
+    });
+  }
+};
+
+exports.getAllUsers = (req, res) => {
+  User.find((err, users) => {
+    if (err) {
+      return res.status(400).json({
+        err
+      });
+    }
+    return res.status(200).json({ users });
+  }).select("name email created updated");
+};
+
+exports.getUser = (req, res) => {
+  // console.log("asd");
+  //console.log(req.profile);
+  return res.status(200).json(req.profile);
+};
+
+exports.updateUser = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "photo caould not be uploaded"
+      });
+    }
+    let user = req.profile;
+    user = _.extend(user, fields);
+    user.updated = Date.now();
+
+    if (files.photo) {
+      user.photo.data = fs.readFileSync(files.photo.path);
+      user.photo.contentType = files.photo.type;
+    }
+
+    user.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        });
+      }
+      user.hashed_password = undefined;
+      user.salt = undefined;
+      res.json(user);
+    });
+  });
+};
+
+exports.userPhoto = (req, res, next) => {
+  if (req.profile.photo.data) {
+    res.set(("content-type", req.profile.photo.contentType));
+    return res.send(req.profile.photo.data);
+  }
+  next();
+};
+
+exports.deleteUser = (req, res) => {
+  let user = req.profile;
+
+  user.delete(err => {
+    if (err) {
+      return res.status(401).json({
+        err: "unauthorized"
+      });
+    }
+    return res.status(200).json({
+      msg: "user successfully deleted"
+    });
+  });
+};
+
+exports.addFollowing = (req, res, next) => {
+  User.findByIdAndUpdate(
+    req.body.userId,
+    {
+      $push: { following: req.body.followId }
+    },
+    (err, res) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      next();
+    }
+  );
+};
+
+exports.addFollower = (req, res) => {
+  User.findByIdAndUpdate(
+    req.body.followId,
+    {
+      $push: { following: req.body.userId }
+    },
+    { new: true }
+  )
+    .populate("following", "_id name")
+    .populate("followers", "_id name")
+    .exec((err, result) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      result.hashed_password = undefined;
+      result.salt = undefined;
+      res.json(result);
+    });
+};
 
 
+exports.removeFollowing = (req, res, next) => {
+    User.findByIdAndUpdate(
+      req.body.userId,
+      {
+        $pull: { following: req.body.unfollowId }
+      },
+      (err, res) => {
+        if (err) {
+          return res.status(400).json({ error: err });
+        }
+        next();
+      }
+    );
+  };
+  
+  exports.removeFollower = (req, res) => {
+    User.findByIdAndUpdate(
+      req.body.unfollowId,
+      {
+        $pull: { following: req.body.userId }
+      },
+      { new: true }
+    )
+      .populate("following", "_id name")
+      .populate("followers", "_id name")
+      .exec((err, result) => {
+        if (err) {
+          return res.status(400).json({ error: err });
+        }
+        result.hashed_password = undefined;
+        result.salt = undefined;
+        res.json(result);
+      });
+  };
+  
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,33 @@
-const express = require('express');
+const express = require("express");
 
-const {requireSignin} = require('../controllers/auth');
-const {userFindById, getAllUsers,getUser,updateUser,deleteUser,userPhoto} = require('../controllers/user')
+const { requireSignin } = require("../controllers/auth");
+const {
+  userFindById,
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+  userPhoto,
+  addFollowing,
+  addFollower,
+  removeFollowing,
+  removeFollower
+} = require("../controllers/user");
 const router = express.Router();
 
-router.get('/users',getAllUsers);
-router.get('/user/:userId',requireSignin,getUser);
-router.put('/user/:userId',requireSignin , updateUser);
-router.delete('/user/:userId',requireSignin,deleteUser );
+router.put("/user/follow", requireSignin, addFollowing, addFollower);
+
+router.put("/user/unfollow", requireSignin, removeFollowing, removeFollower);
+
+router.get("/users", getAllUsers);
+router.get("/user/:userId", requireSignin, getUser);
+router.put("/user/:userId", requireSignin, updateUser);
+router.delete("/user/:userId", requireSignin, deleteUser);
 
 //for photo
-router.get('/user/photo/:userId',userPhoto)
+router.get("/user/photo/:userId", userPhoto);
 
 // any rote containing userById param will first execute user find by id
-router.param("userId" , userFindById )
+router.param("userId", userFindById);
 
-module.exports =router;
+module.exports = router;
